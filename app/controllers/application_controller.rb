025a45f69372195cd899fbbd5e64b67bc2ce7f37
aranza <aranza@michelada.io>
2018-05-08 00:34:18 -0500

    First commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  # protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+end
